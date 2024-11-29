4221cee0adcb404561ce432672d7cde1cd5d08e2
KKarafiloski <70888362+KKarafiloski@users.noreply.github.com>
2024-11-29 12:50:36 +0100

    Add Task 4 draft - Two-way data binding

---
+++ code/frontend/src/components/Task4.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Task4Input from './Task4Input.vue';
+const name = ref('');
+</script>
+
+<template>
+    <div>
+        <h1>Task 4: Two-Way Data Binding</h1>
+        <p>Enter your name:</p>
+        <Task4Input v-model="name"/>
+        <p>Hello, {{ name }}!</p>
+    </div>
+</template>
