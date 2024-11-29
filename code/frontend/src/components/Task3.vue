15e4b53ad9c385978d5a9c43c841e12875493a80
KKarafiloski <70888362+KKarafiloski@users.noreply.github.com>
2024-11-29 12:50:03 +0100

    Add Task 3 draft - Custom component

---
+++ code/frontend/src/components/Task3.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Task3Card from './Task3Card.vue';
+</script>
+
+<template>
+    <div>
+        <h1>Task 3: Custom Component</h1>
+        <Task3Card title="Vue is awesome!" content="Vue makes frontend development simpler." />
+    </div>
+</template>
