d1056d5cb3a14800ff04d387e754152fd4663c6e
Luca v. d. W. <l.vanderwill@student.fontys.nl>
2024-12-13 14:11:52 +0100

    Use task wrapper for task 1
    Co-authored-by: KKarafiloski <70888362+KKarafiloski@users.noreply.github.com>
    Co-authored-by: MiroMiloloza <70884219+MiroMiloloza@users.noreply.github.com>

--- code/frontend/src/components/Task1.vue
+++ code/frontend/src/components/Task1.vue
@@ -1,18 +1,31 @@
 <script lang="ts" setup>
-import { ref, reactive } from 'vue';
-const message = ref('Hello, Vue.js!');
-const state = reactive({ counter: 0 });
+import TaskWrapper from "./TaskWrapper.vue"
+import { ref, reactive } from "vue"
+
+const message = ref('Hello, Vue.js!')
+const state = reactive({ counter: 0 })
 
 const incrementCounter = () => {
-    state.counter++;
-};
+    state.counter++
+}
 </script>
 
 <template>
-    <div>
-        <h1>Task 1: Vue Basics</h1>
-        <p>Message: {{ message }}</p>
-        <p>Counter: {{ state.counter }}</p>
-        <button @click="incrementCounter">Increment Counter</button>
-    </div>
+    <TaskWrapper>
+        <template #header>
+            <h1>Task 1: Vue Basics</h1>
+            <p>
+                hello welcome to task 1 you are well on your way to becoming an osu! rhythm champion
+            </p>
+        </template>
+        <template #default>
+            <p>Message: {{ message }}</p>
+            <p>Counter: {{ state.counter }}</p>
+            <button @click="incrementCounter">Increment Counter</button>
+        </template>
+    </TaskWrapper>
 </template>
+
+<style scoped>
+
+</style>
