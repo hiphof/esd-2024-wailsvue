72ca152af36c1f4de2032203b486f17b695d13e4
Luca v. d. W. <l.vanderwill@student.fontys.nl>
2024-12-13 14:11:16 +0100

    Add home to router
    Co-authored-by: KKarafiloski <70888362+KKarafiloski@users.noreply.github.com>
    Co-authored-by: MiroMiloloza <70884219+MiroMiloloza@users.noreply.github.com>

--- code/frontend/src/router.ts
+++ code/frontend/src/router.ts
@@ -1,11 +1,13 @@
 import { createWebHistory, createRouter } from "vue-router"
 
+import Home from "./components/Home.vue"
 import Task1 from "./components/Task1.vue"
 import Task2 from "./components/Task2.vue"
 import Task3 from "./components/Task3.vue"
 import Task4 from "./components/Task4.vue"
 
 const routes = [
+	{ path: "/", component: Home },
 	{ path: "/task-1", component: Task1 },
 	{ path: "/task-2", component: Task2 },
 	{ path: "/task-3", component: Task3 },
