4b4ddcf46b35d69ba2da28ff79b354d53a1a6a7d
Luca v. d. W. <l.vanderwill@student.fontys.nl>
2024-12-13 12:37:16 +0100

    Fix port configuration for startup
    Co-authored-by: KKarafiloski <70888362+KKarafiloski@users.noreply.github.com>
    Co-authored-by: MiroMiloloza <70884219+MiroMiloloza@users.noreply.github.com>

--- code/frontend/vite.config.ts
+++ code/frontend/vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue"
 export default defineConfig({
     plugins: [ vue() ],
     server: {
+        host: "127.0.0.1",
         watch: {
             usePolling: true, // Essential for Docker file system changes
         },
