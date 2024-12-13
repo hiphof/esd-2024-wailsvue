edf5a2b353f9c97e95c9ff1d60a5c5e86b579e59
Luca v. d. W. <l.vanderwill@student.fontys.nl>
2024-12-13 14:09:07 +0100

    Add TaskWrapper component
    Co-authored-by: KKarafiloski <70888362+KKarafiloski@users.noreply.github.com>
    Co-authored-by: MiroMiloloza <70884219+MiroMiloloza@users.noreply.github.com>

---
+++ code/frontend/src/components/TaskWrapper.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+// empty
+</script>
+
+<template>
+    <div class="container">
+        <div class="header">
+            <slot name="header"></slot>
+        </div>
+        <div class="content">
+            <div class="floating-card">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+.header {
+    /* empty */
+}
+
+.floating-card {
+    background: #202020;
+    border-radius: 0.5rem;
+    border: 2px #242424 solid;
+    padding: 2rem;
+}
+
+.content {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 2rem;
+}
+</style>
