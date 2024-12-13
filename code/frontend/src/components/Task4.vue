c0f3178fdbb7321a0510f65a256e1bfaac3179fd
Luca v. d. W. <l.vanderwill@student.fontys.nl>
2024-12-13 14:12:13 +0100

    Use task wrapper for task 4
    Co-authored-by: KKarafiloski <70888362+KKarafiloski@users.noreply.github.com>
    Co-authored-by: MiroMiloloza <70884219+MiroMiloloza@users.noreply.github.com>

--- code/frontend/src/components/Task4.vue
+++ code/frontend/src/components/Task4.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
+import TaskWrapper from "./TaskWrapper.vue"
 import { ref } from 'vue';
 import Task4Input from './Task4Input.vue';
 const name = ref('');
 </script>
 
 <template>
-    <div>
-        <h1>Task 4: Two-Way Data Binding</h1>
-        <p>Enter your name:</p>
-        <Task4Input v-model="name"/>
-        <p>Hello, {{ name }}!</p>
-    </div>
+    <TaskWrapper>
+        <template #header>
+            <h1>Task 4: Two-Way Data Binding</h1>
+            <p>
+                hello welcome to task 4 you are well on your way to becoming an osu! rhythm champion
+            </p>
+        </template>
+        <template #default>
+            <p>Enter your name:</p>
+            <Task4Input v-model="name"/>
+            <p>Hello, {{ name }}!</p>
+        </template>
+    </TaskWrapper>
 </template>
