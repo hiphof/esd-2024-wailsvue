d6b45ca3f9062498b2b1816010885d3aec7c4370
Luca v. d. W. <l.vanderwill@student.fontys.nl>
2024-12-20 15:52:31 +0100

    Update practical task 1
    Co-authored-by: KKarafiloski <70888362+KKarafiloski@users.noreply.github.com>

--- code/frontend/src/tasks/task-1/Task1.vue
+++ code/frontend/src/tasks/task-1/Task1.vue
@@ -1,20 +1,47 @@
 <script setup lang="ts">
-import { reactive, ref } from "vue"
+import { ref } from "vue"
 
-const message = ref("Hello, Vue.js!")
-const state = reactive({ counter: 0 })
+/* TODO 1.1: add a counter 'state' by using Vue's ref */
 
-const incrementCounter = () => {
-	state.counter++
-}
+/* TODO 1.2: add a function which increments the counter */
+
 </script>
 
 <template>
-	<p>Message: {{ message }}</p>
-	<p>Counter: {{ state.counter }}</p>
-	<button @click="incrementCounter">Increment Counter</button>
+	<!-- TODO 1.3: display the counter -->
+	<p>Counter: 0</p>
+
+	<!-- TODO 1.4: add click listener to call your function -->
+	<button>Increment Counter</button>
 </template>
 
 <style scoped>
+/* NOTE:
+	scoped styling means that
+	all of the following ONLY applies to this component
+*/
+p {
+	font-size: x-large;
+}
 
+button {
+	font-size: large;
+	background-color: hsl(168, 76%, 42%);
+	border: none;
+	padding: 1rem;
+	border-radius: 0.25rem;
+	transition: all 0.1s ease-out;
+	cursor: pointer;
+	user-select: none;
+}
+
+button:hover {
+	background-color: hsl(168, 76%, 47%);
+	box-shadow: hsl(168, 76%, 47%, 0.366) 0 0 1rem;
+}
+
+button:active {
+	background-color: hsl(168, 76%, 37%);
+	transform: scale(0.95);
+}
 </style>
