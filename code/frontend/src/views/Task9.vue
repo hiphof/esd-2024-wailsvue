5db7aae1c69a88159308875f490dd859b1a43a2a
KKarafiloski <70888362+KKarafiloski@users.noreply.github.com>
2025-01-05 20:07:35 +0100

    Add Task 9 description

--- code/frontend/src/views/Task9.vue
+++ code/frontend/src/views/Task9.vue
@@ -8,7 +8,17 @@ import Task9 from "../tasks/task-9/Task9.vue"
 		<template #header>
 			<h1>Task 9: Two-Way Data Binding</h1>
 			<p>
-				hello welcome to task 9 you are well on your way to becoming an osu! rhythm champion
+				Two-way data binding? What's that?
+			</p>
+			<p>
+				This task should show you that data can be manipulated both ways using 'v-model'. 
+				From a user and programatically.
+			</p>
+			<p>
+				Make it possible to automatically update the quote by typing it yourself
+				and replace that quote with a random Skyrim quote by pressing the button.
+				Yes, that's a little weird example but it's to show your input automatically updates as well
+				as the button press. 
 			</p>
 		</template>
 		<template #default>
