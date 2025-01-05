426e133c9f45897993893f37859dbfba198dc38b
KKarafiloski <70888362+KKarafiloski@users.noreply.github.com>
2025-01-05 20:06:51 +0100

    Add Task 4 description

--- code/frontend/src/views/Task4.vue
+++ code/frontend/src/views/Task4.vue
@@ -8,7 +8,11 @@ import Task4 from "../tasks/task-4/Task4.vue"
 		<template #header>
 			<h1>Task 4: Conditional Rendering</h1>
 			<p>
-				hello welcome to task 4 you are well on your way to becoming an osu! rhythm champion
+				Welcome to task 4, where we will check out conditional rendering!
+			</p>
+			<p>
+				The buttons don't work like the should, make the buttons show the status message using conditionals.
+				Check out 'v-if', 'v-else-if' and 'v-else'
 			</p>
 		</template>
 		<template #default>
